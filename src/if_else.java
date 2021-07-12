31260a330b89c2e28b429a67b7c1f50e2445f6ee
Aman Maurya <aman.maurya@globallogic.com>
2021-07-12 09:57:54 +0530

    if else program added

---
+++ src/if_else.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc =new Scanner(System.in);
+		if(sc.nextInt()%2==0) {
+			System.out.println("Even Number");
+		}
+		else {
+			System.out.println("Odd Number");
+		}
+	}
+}
